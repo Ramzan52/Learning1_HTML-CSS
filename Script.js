53f1e6d4e3ca3cc44d8ea301c686da13ec419c9a
Ramzan52 <62610512+Ramzan52@users.noreply.github.com>
2020-06-23 21:33:07 +0500

    bindings

--- Script.js
+++ Script.js
@@ -1,10 +1,12 @@
-function clicked() {
-  alert("coming soon!!!");
-}
-function clicked1() {
-  alert("Page under construction!");
-}
+window.onload = function () {
+  function clicked() {
+    alert("coming soon!!!");
+  }
+  function clicked1() {
+    alert("Page under construction!");
+  }
 
-function loadPage() {
-  window.location = "bucket.html";
-}
+  function loadPage() {
+    window.location = "bucket.html";
+  }
+};
