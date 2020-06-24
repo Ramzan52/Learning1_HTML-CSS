29dfe667e9e1a28f9defaaa32b94a3f06466eff0
Ramzan52 <62610512+Ramzan52@users.noreply.github.com>
2020-06-24 21:50:06 +0500

    final

--- Script.js
+++ Script.js
@@ -1,12 +1,10 @@
-window.onload = function () {
-  function clicked() {
-    alert("coming soon!!!");
-  }
-  function clicked1() {
-    alert("Page under construction!");
-  }
+function clicked() {
+  alert("coming soon!!!");
+}
+function clicked1() {
+  alert("Page under construction!");
+}
 
-  function loadPage() {
-    window.location = "bucket.html";
-  }
-};
+function loadPage() {
+  window.location = "bucket.html";
+}
