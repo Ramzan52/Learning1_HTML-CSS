5eae1e3805290b4f0f1c05a07c07db08e5ecc5ed
Ramzan52 <62610512+Ramzan52@users.noreply.github.com>
2020-07-04 21:47:56 +0500

    commit

--- js/bucket.js
+++ js/bucket.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   let bucket = JSON.parse(localStorage.getItem("Bucket")) || [];
   bucket.map((single) => {
-    $("#list")
-      .append(`<li>${single.name}     ${single.price}  <button type="button" class="btn btn-danger" onclick="remove()">Remove</button>
-     </li>`);
+    $("#menu")
+      .append(`<table><tr><td id="firstrow">${single.name}</td>   <td id="secondrow">  ${single.price}</td><td id="thirdrow"><button type="button" class="btn btn-danger" onclick="remove()">Remove</button></td></tr>
+    </table> `);
   });
 });
 function remove() {
-  console.log("AA");
+  localStorage.removeItem("Bucket");
 }
