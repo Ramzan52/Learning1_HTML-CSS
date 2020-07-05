d237b5aade180e8084870fbbcd10a9ff977947a0
Ramzan52 <62610512+Ramzan52@users.noreply.github.com>
2020-07-05 17:27:36 +0500

    commit

--- js/bucket.js
+++ js/bucket.js
@@ -2,7 +2,17 @@ $(document).ready(function () {
   let bucket = JSON.parse(localStorage.getItem("Bucket")) || [];
   bucket.map((single) => {
     $("#menu")
-      .append(`<table><tr><td id="firstrow">${single.name}</td>   <td id="secondrow">  ${single.price}</td><td id="thirdrow"><button type="button" class="btn btn-danger" onclick="remove()">Remove</button></td></tr>
+      .append(`<table><tr><td id="firstrow">${single.name}</td>   <td id="secondrow" class="second">  ${single.price}</td><td id="thirdrow"><button type="button" class="btn btn-danger" onclick="remove()">Remove</button></td></tr>
     </table> `);
   });
-});
+  TotalPriceCalc();
+ 
+
+function TotalPriceCalc() {
+  var total = 0;
+  $(".second").each(function () {
+    total += parseFloat($(this).text());
+  });
+  $("#input").val(total);
+  console.log(total);
+}
